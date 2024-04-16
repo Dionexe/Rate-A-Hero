d35f690746d160d39cf4b4100c1444ed8ecd50c8
Dion Wells <dionwells@Dions-MacBook-Pro.local>
2024-04-15 21:07:05 -0700

    Search Component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,21 @@ import Search from './components/Search'
 
 function App() {
   return (
-      <div className="App">
-      <Search />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
