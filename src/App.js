cc277842aa6dd6c20ff1b4ef5dd10d8eb56298a1
Zeth Thomas <zeththomas89@gmail.com>
2024-04-15 20:55:58 -0700

    Home page added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Rate A Hero</h1>
+        <div>
+          <img />
+          <div className="form-group">
+            <label htmlFor="Hero Search">Search For A Hero</label>
+            <input className="form-control" id="search" name="search" />
+          </div>
+        </div>
+        <a href="/characters">
+          <button className="btn-primary">Search</button>
+        </a>   
       </header>
     </div>
   );
