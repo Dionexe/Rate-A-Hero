31fe935acd1629bd3b9b6be6fa247e0804e2cd61
Daijon Bourne <Daijonbourne1005@gmail.com>
2024-04-18 19:55:50 -0700

    Fix route on App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Main } from './Main';
+import { Main } from './components/Main';
 // import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Marvel } from './Marvel';
