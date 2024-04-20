a0778a299aeff2d4b13cbcd4bd0e54979dd5c125
Malayai Wilson <codermalayaiwilson@gmail.com>
2024-04-20 08:01:48 -0700

    starting read me

---
+++ src/CardComponent.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './App.css'; 
+
+export const CardComponent = ({ data }) => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      {
+        (data) ? (
+          data.map(item => (
+            <Card 
+              key={item.id}
+              className="card_container" 
+              style={{ width: '18rem', cursor: 'pointer' }} 
+              onClick={() => navigate(`/${item.id}`)}
+            >
+              <Card.Title className='card-title'>{item.name}</Card.Title>
+              <Card.Img className="img-fluid" variant="top" src={`${item.thumbnail.path}.${item.thumbnail.extension}`} alt="Marvel" style={{ maxWidth: '100%', height: 'auto' }}/>
+              <Card.Body>
+                <Card.Text>
+                {item.description ? item.description : 'No description available'}
+                </Card.Text>
+                <Button className="card-button" variant="primary">Rate & Comment</Button>
+              </Card.Body>
+            </Card>
+          ))
+        ) : ""
+      }
+    </>
+  );
+};
---
+++ src/Main.js
@@ -0,0 +1,58 @@
+import React from "react"
+import { CardComponent } from "./CardComponent"
+import axios from "axios"
+import { useState } from "react"
+import { useEffect } from "react"
+import md5 from 'md5';
+
+
+export const Main = () => {
+  const publicKey = process.env.REACT_APP_PUBLIC_KEY;
+  const privateKey = process.env.REACT_APP_PRIVATE_KEY;
+  const timeStamp = new Date().getTime().toString();
+  const hash = md5(timeStamp + privateKey + publicKey);
+
+  const [url,setUrl]=useState(`https://gateway.marvel.com/v1/public/characters?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`);
+  const [item,setItem]=useState();
+  const [search,setSearch]=useState("");
+  useEffect(()=>{
+    const fetch=async()=>{
+      const res=await axios.get(url)
+      setItem(res.data.data.results);
+    }
+    fetch();
+  },[url])
+  
+  const searchMarvel=()=>{
+    setUrl(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${search}&ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`);
+  }
+
+  return (
+    <>
+        <div className="header">
+            <div className="bg">
+            </div>
+            <div className="search-bar">
+                <img className="Marvel" src="/marvel.jpg" alt="Marvel Logo" />
+                <input
+                    type="search"
+                    placeholder="Search Here"
+                    className="search"
+                    onChange={(e) => setSearch(e.target.value)}
+                    onKeyDown={(e) => {
+                      if (e.key === 'Enter') {
+                        searchMarvel();
+                      }
+                    }}
+                 />
+            </div>
+        </div>
+       <div className="content">
+         
+        {
+          (!item)?<p className="Null">Not Found</p>:<CardComponent data={item}/>
+        }
+       </div>
+    </>
+  )
+}
